923dab72d4e36f72ce75e27dc5c4d43002ba0599
LuK1337 <priv.luk@gmail.com>
2025-01-22 15:07:18 +0100

    odin2thor: Set default bluetooth name
    Change-Id: Iecbabba448d0b2b31ca2e1399301799cd546492c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,5 +8,9 @@ DEVICE_PATH := device/ayn/odin2thor
 # Include the common OEM chipset BoardConfig.
 include device/ayn/qcs8550-common/BoardConfigCommon.mk
 
+# Properties
+DEVICE_PROPERTIES_PATH := $(DEVICE_PATH)/properties
+TARGET_VENDOR_PROP += $(DEVICE_PROPERTIES_PATH)/vendor.prop
+
 # Include the proprietary files BoardConfig.
 include vendor/ayn/odin2thor/BoardConfigVendor.mk
