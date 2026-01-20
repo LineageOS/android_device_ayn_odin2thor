90ae341081a4e3449a7032ea4d45b21446b5a085
AnierinB <anierin@evolution-x.org>
2026-01-20 04:17:27 -0800

    odin2thor: Initial skeleton tree
    Change-Id: Ic1331056aa058487059f0880bf1f2edb9ad10bfe
    Signed-off-by: AnierinB <anierin@evolution-x.org>

---
+++ lineage_odin2thor.mk
@@ -0,0 +1,27 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from odin2thor device
+$(call inherit-product, device/ayn/odin2thor/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+
+PRODUCT_NAME := lineage_odin2thor
+PRODUCT_DEVICE := odin2thor
+PRODUCT_MANUFACTURER := AYN
+PRODUCT_BRAND := AYN
+PRODUCT_MODEL := Thor
+PRODUCT_SYSTEM_DEVICE := thor
+PRODUCT_SYSTEM_NAME := Thor
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    SystemDevice=Thor \
+    SystemName=thor \
+    DeviceProduct=odin2thor
