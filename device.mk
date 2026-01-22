c1104e47108725c3934aa1ea2a070b3748a68c67
AnierinB <anierin@evolution-x.org>
2026-01-22 11:01:32 +0000

    odin2thor: Setup device specific overlays
    Change-Id: Ib4bbe30cd9f12c6b0a4e3019db8e26ff0bdb5703
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- device.mk
+++ device.mk
@@ -3,6 +3,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Overlay
+PRODUCT_PACKAGES += \
+    Frameworks-Thor-Overlay \
+    Settings-Thor-Overlay \
+    SettingsProvider-Thor-Overlay
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
